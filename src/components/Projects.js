472c481817c7dc00bc41c05d0ae1ebf72cf9d15c
Erik Correa <erikcorrea444@gmail.com>
2021-06-23 11:46:18 -0500

    restyled project component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,33 +2,38 @@ import React from 'react';
 
 const Projects = () => {
     return (
-			<div className='project-container'>
-				<h1 id='project-h1'>Projects</h1>
-				<div className='project-cards'>
-					<div className='project-card-one'>
-						<div className='card-background-one'></div>
-						<div className='card-btns'>
-							<a href="https://github.com/erikcorea/flash-card"><button className='project-github-btn'>gitHub</button></a>
-							<a href="https://estudiarcards.netlify.app/"><button className='project-live-btn'>Live</button></a>
-						</div>
-					</div>
-					<div className='project-card-two'>
-						<div className='card-background-two'></div>
-						<div className='card-btns'>
-							<a href="https://github.com/erikcorea/news-project2"><button className='project-github-btn'>gitHub</button></a>
-							<button className='project-live-btn'>Live</button>
-						</div>
-					</div>
-					<div className='project-card-three'>
-						<div className='card-background-three'></div>
-						<div className='card-btns'>
-							<a href="https://github.com/Team-Orange4"><button className='project-github-btn'>gitHub</button></a>
-							<a href="http://team-orange4.herokuapp.com/"><button className='project-live-btn'>Live</button></a>
-						</div>
+		<>
+			<h1 id="project-h1">Projects</h1>
+			<div className="card-container">
+				<div className="card">
+					<div className="card-image card-one"></div>
+					<div className="card-btns">
+						<a href="https://github.com/erikcorea/flash-card">GitHub</a>
+						<a href="https://estudiarcards.netlify.app/">Live</a>
 					</div>
 				</div>
-			</div>
+				<div className="card">
+					<div className="card-image card-two"></div>
+					<div className="card-btns">
+						<a href="https://github.com/erikcorea/news-project2">GitHub</a>
+						<a href="">Live</a>
+					</div>
+				</div>
+
+				<div className="card">
+					<div className="card-image card-three"></div>
+					<div className="card-btns">
+						<a href="https://github.com/Team-Orange4">GitHub</a>
+						<a href="http://team-orange4.herokuapp.com/">Live</a>
+					</div>
+				</div>
+			</div> 
+		</>
 		);
 };
+
+
+
+
 
 export default Projects;
